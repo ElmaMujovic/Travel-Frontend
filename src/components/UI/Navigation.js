b0f0d67c8a0938fcbbc0bae8ec6030906ef38702
ElmaMujovic <elma.mujov1c1@gmail.com>
2024-09-01 22:43:04 +0200

    sve pre rezervacije

--- src/components/UI/Navigation.js
+++ src/components/UI/Navigation.js
@@ -1,52 +1,57 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useMainContext } from '../../context/main-context'
-import './Navigation.css'
-import axios from 'axios'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { useMainContext } from '../../context/main-context';
+import './Navigation.css';
+import axios from 'axios';
 
 const Navigation = () => {
     const { user, onSetUserHandler } = useMainContext();
     const navigate = useNavigate();
+
     const logoutHandler = () => {
-        onSetUserHandler(null)
-        axios.defaults.headers.common[
-            "Authorization"
-          ] = '';
+        onSetUserHandler(null);
+        axios.defaults.headers.common["Authorization"] = '';
         localStorage.removeItem("user");
         navigate('/');
-        console.log("user", user)
-        //user && user.role === "A" ?
+        console.log("user", user);
     }
 
     return (
         <div className='navigation'>
-            <ul>
+            <ul className='nav-links'>
                 <Link to='/'>Početna</Link>
-                {/* <Link to='/about'>About</Link> */}
-                {user && user.role === 'User'? <Link to="/about">O nama</Link>:""}
+                {user && user.role === 'User' ? <Link to="/about">O nama</Link> : ""}
                 {!user && <Link to='/login'>Login</Link>}
-                {user && <button onClick={logoutHandler} style={{textDecoration:"none", border:"none", backgroundColor:"rgb(199, 209, 245)", color:"white", marginTop:"10px", fontSize:"15px", marginRight:"6px"}}>Logout</button>}
-                {/* <Link to='/contact'>Kontakt</Link> */}
                 {user && (user.role === 'User' || user.role === 'Moderator') ? <Link to="/fleet">Destinacije</Link> : ""}
+                {user && user.role === 'Moderator' ? <Link to="/create-destination">Kreiraj destinaciju</Link> : ""}
+                {user && user.role === 'Admin' ? <Link to="/create-package">Kreiraj paket</Link> : ""}
+                {user && user.role === 'Admin' ? <Link to="/create-package-destinacija">Destinacije paketa</Link> : ""}
+                {user && user.role === 'Admin' ? <Link to="/create-list">Kreiraj listu</Link> : ""}
+                {user && user.role === 'Admin' ? <Link to="/hotel">Hotel</Link> : ""}
+                {user && <Link to='/profile'>Moj profil</Link>}
 
-                {user && user.role === 'Moderator'? <Link to="/create-destination">Kreiraj destinaciju</Link>:""}
-                {user && user.role === 'Admin'? <Link to="/create-package">Kreiraj paket</Link>:""}
-                {user && user.role === 'Admin'? <Link to="/create-package-destinacija">Destinacije paketa</Link>:""}
-                {user && user.role === 'Admin'? <Link to="/create-list">Kreiraj listu</Link>:""}
-
-                {user && user.role === 'Admin'? <Link to="/hotel">Hotel</Link>:""}
-
-
-                {user &&  <Link to='/profile'>Moj profil</Link>}
-
-                <li className='second-part'><b>TravelApp</b></li>
-                
-
-
+                <li className='second-part'>
+                    {user && 
+                        <button 
+                            onClick={logoutHandler} 
+                            style={{ 
+                                backgroundColor: 'transparent', 
+                                border: 'none', 
+                                cursor: 'pointer',
+                                display: 'flex',
+                                alignItems: 'center'
+                            }}
+                        >
+                            <FontAwesomeIcon icon={faSignOutAlt} size="lg" color="white" />
+                            <b className='travelapp-text'>TravelApp</b>
+                        </button>
+                    }
+                </li>
             </ul>
-           
         </div>
     )
 }
 
-export default Navigation
+export default Navigation;
